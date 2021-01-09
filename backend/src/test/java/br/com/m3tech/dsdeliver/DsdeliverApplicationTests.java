3e5d26d70abf85d3ee636ecbd1ef9e52fb0077a4
mackristofer <mackristofer@gmail.com>
2021-01-09 00:43:03 -0300

    Project created

---
+++ backend/src/test/java/br/com/m3tech/dsdeliver/DsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.m3tech.dsdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
