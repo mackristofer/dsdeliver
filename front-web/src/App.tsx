9a81eca9ddaa2f7560c67e8974096a6d3d56a45a
mackristofer <mackristofer@gmail.com>
2021-01-10 01:07:54 -0300

    Home PG

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -2,15 +2,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 
 function App() {
   return (
-    <div>
+    <>
     <Navbar />
+    <Home />
   
    
-    </div>
+    </>
   );
 }
 
