8f87bc4650b4c17921a9f6578cc711ccbca3ed89
mackristofer <mackristofer@gmail.com>
2021-01-16 03:36:11 -0300

    Finish app

--- front-mobile/src/OrderDetails/index.tsx
+++ front-mobile/src/OrderDetails/index.tsx
@@ -1,7 +1,7 @@
 
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, View, Text, Alert } from 'react-native';
+import { StyleSheet, View, Text, Alert, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
 import { confirmDelivery } from '../api';
 import Header from '../Header';
@@ -37,12 +37,16 @@ export default function OrderDetails( {route}: Props) {
     })
   }
 
+  const handleStartNavigation = () =>{
+      Linking.openURL(`https://www.google.com/maps/dir/?api=1&travelmode=driving&dir_action=navigate&destination=${order.latitude},${order.longitude}`)
+  }
+
   return (
     <>
     <Header />
     <View style={styles.container}>
         <OrderCard order={order} />
-        <RectButton style={styles.button}><Text style={styles.buttonText}>Iniciar navegação.</Text></RectButton>
+        <RectButton style={styles.button} onPress={handleStartNavigation}><Text style={styles.buttonText}>Iniciar navegação.</Text></RectButton>
         <RectButton style={styles.button} onPress={handleConfirmDelivery}><Text style={styles.buttonText}>Confirmar entrega</Text></RectButton>
         <RectButton style={styles.button} onPress={handleOnCancel}><Text style={styles.buttonText}>Cancelar entrega</Text></RectButton>
     </View>
